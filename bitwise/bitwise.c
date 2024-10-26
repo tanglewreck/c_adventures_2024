10ef2d52d824369bbf2c3874701cd72c5b25f5d0
mier <mier@tanglewreck.mier.org>
2024-10-26 14:08:15 +0200

    Bitwise operations. Initial commit

---
+++ bitwise/bitwise.c
@@ -0,0 +1,25 @@
+/*******************************************************************
+ * bitwise -- program to test various bitwise operations
+ *
+ * Author:
+ *      mier
+ * Date:
+ *      2024-10-26
+ * Purpose: 
+ *      Learning bitwise operations
+ * Usage: 
+ *      Run the program and various messages appear.
+ * *******************************************************************/
+#include <stdio.h>
+
+int a, b;
+int main() {
+    int a, b;
+    a = 2;
+    b = 4;
+    printf("%d, %d\n", a, b);
+    printf("%d & %d = %d\n", a, b, a & b);
+    return(0);
+}
+
+
