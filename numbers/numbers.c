f266b4c7a1fcd605dc8ad833051c932250ded43c
mier <mier@tanglewreck.mier.org>
2024-10-26 22:37:14 +0200

    Added numerical conversions

--- numbers/numbers.c
+++ numbers/numbers.c
@@ -13,6 +13,7 @@
 #include <stdio.h>
 #include <string.h>
 
+
 int main() {
 
     int c = 0;
@@ -22,6 +23,9 @@ int main() {
     double d1 = 10 / 3; 
     double d1 = 10 / 3; 
     */
+    int i1 = 100;
+    int i2 = 10;
+    int i3 = 3;
 
     printf("int c: %d\n", c);
     printf("int 10 / 3: %d\n", 10 / 3);
@@ -35,5 +39,12 @@ int main() {
     printf("long f 10.0 / 3.0: %lf\n", 10.0 / 3.0); 
     printf("long g 10.0 / 3.0: %lg\n", 10.0 / 3.0); 
     printf("long G 10.0 / 3.0: %lG\n", 10.0 / 3.0); 
-    printf("double d1 %lf\n", (double) 10.0 / 3.0); 
+    printf("double 10.0 / 3.0: %lf\n", (double) 10.0 / 3.0); 
+    printf("long 30.20f right adjusted 10.0 / 3.0: %30.20lf\n", 10.0 / 3.0); 
+    printf("long 30.20f left adjusted 10.0 / 3.0: %-30.20lf\n", 10.0 / 3.0); 
+    printf("hexadecimal 10 / 3: %x\n", (unsigned int) 10 / 3); 
+    printf("dec %u / %u: %u\n", i1, i3, i1 / i3);
+    printf("dec %u / %u: %u\n", i2, i3, i2 / i3);
+    printf("hex %u / %u: %x\n", i1, i3, i1 / i3);
+    printf("hex %u / %u: %x\n", i2, i3, i2 / i3);
 }
